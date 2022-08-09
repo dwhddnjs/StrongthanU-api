edb51eac158e4e5c0ab918b46184890ddb145be4
Jongwon <per1215@naver.com>
2022-08-10 00:38:05 +0900

    🎨 env로 변경합니다

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const typeDefs = require("./graphql/typeDefs");
 
 require("dotenv").config();
 
-// const URL = process.env.DB_URL;
-
-// const PORT = process.env.PORT;
-
-// console.log("URL: ", URL);
-// console.log("PORT: ", PORT);
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -23,16 +16,13 @@ const server = new ApolloServer({
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://per1215:a102030@first-project.oxmyz.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("몽고디비랑 연결 되었습니다");
-    return server.listen({ port: 4000 });
+    return server.listen({ port: process.env.PORT || 4000 });
   })
   .then((res) => {
     console.log(`서버가 ${res.url}로 실행중입니다`);
