9d9483144c28558862e7180edae5feb608b7ce89
Jongwon <per1215@naver.com>
2022-08-10 00:06:51 +0900

    🎨 env포트번호도 제거합니다

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose
   )
   .then(() => {
     console.log("몽고디비랑 연결 되었습니다");
-    return server.listen({ port: PORT || 4000 });
+    return server.listen({ port: 4000 });
   })
   .then((res) => {
     console.log(`서버가 ${res.url}로 실행중입니다`);
